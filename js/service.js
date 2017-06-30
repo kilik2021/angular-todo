eb0bd7e4bb940e1f7e4d290849bf1c28cf7b8699
Justin Robinson <kilik2021@users.noreply.github.com>
2017-06-30 15:37:34 +0100

    Added Todo Functionality

--- js/service.js
+++ js/service.js
@@ -1,10 +1,24 @@
 angular.module('UserService', [])
     .factory('UserAIPService', function($http) {
-
         UserAIPService = {
             callAPI: function(url, data) {
                 return $http.post(url, data);
             }
         };
         return UserAIPService;
+    });
+
+angular.module('TodoService', [])
+    .factory('TodoAPIService', function($http) {
+        TodoAPIService = {
+            getTodos: function(url, data, token) {
+                var header = "Authorization: JWT " + token;
+                return $http.get(url, {params:{"username": data}}, header);
+            },
+            createTodo: function(url, data, token) {
+                var header = "Authorization: JWT " + token;
+                return $http.post(url, data, header);
+            }
+        };
+        return TodoAPIService;
     });
